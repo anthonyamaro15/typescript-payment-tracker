e5450fdca68f8f05d14b64d3dacfa58e58e96ae3
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-09-13 20:04:38 -0700

    displaying data now

--- src/components/displayInformation.tsx
+++ src/components/displayInformation.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import SlideTracks from "./SlideTracks";
 
 interface Values {
+  id: number;
   card_type: string;
   account: string;
   amount: number;
@@ -10,10 +12,51 @@ interface Values {
 interface Props {
   data: Values[];
 }
+
 const DisplayInformation: React.FC<Props> = ({ data }) => {
+  const [types, setTypes] = useState([
+    "discover it",
+    "amex cash rewards",
+    "amex cash back",
+  ]);
+
+  const discover = data.filter((t) => t.card_type === types[0]);
+  const amex_rewards = data.filter((t) => t.card_type === types[1]);
+  const amex_cash = data.filter((t) => t.card_type === types[2]);
+
+  //   for (let i = 0; i < data.length; i++) {
+  //     console.log("here ", data[i]);
+  //   }
+  function displayInfo(cardName: Values[]) {
+    return (
+      cardName.length && (
+        <div>
+          {cardName.map((t) => (
+            <SlideTracks key={t.id} t={t} />
+          ))}
+        </div>
+      )
+    );
+  }
+
+  const displayDiscover = displayInfo(discover);
+  const displayAmexReward = displayInfo(amex_rewards);
+  const displayCash = displayInfo(amex_cash);
+
   return (
     <div className="DisplayInformation">
-      <h3>hello</h3>
+      <div className="card-names">
+        <h3>discover it</h3>
+        {displayDiscover}
+      </div>
+      <div className="card-names">
+        <h3>Amex cash rewards</h3>
+        {displayAmexReward}
+      </div>{" "}
+      <div className="card-names">
+        <h3>Amex cash</h3>
+        {displayCash}
+      </div>
     </div>
   );
 };
